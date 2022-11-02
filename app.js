f76a48be738c85293b2fa6ac6f10a88a2d4f855e
Jugya K. Gogoi <crjugya123@gmail.com>
2022-11-02 23:01:16 +0530

    updated the ui

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 const password = pass.getPass();
-mongoose.connect(`mongodb+srv://Jugya07:${password}@cluster0.0gphwix.mongodb.net/toDoListDB`, {
+mongoose.connect(`mongodb://localhost:27017/toDoListDB`, {
   useNewUrlParser: true,
 });
 
@@ -78,11 +78,11 @@ app.post("/delete",  (req, res) =>{
   res.redirect("/");
 });
 
-let port = process.env.PORT;
-if (port == null || port == "") {
-  port = 3000;
-}
+// let port = process.env.PORT;
+// if (port == null || port == "") {
+//   port = 3000;
+// }
 
-app.listen(port,  () => {
+app.listen(3000, () => {
   console.log("Server is running");
 });
